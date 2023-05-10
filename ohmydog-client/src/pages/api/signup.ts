45d2dcb7d93da8eee4848b0798262cdf55ff64a8
martLeand <mart.leand.99@gmail.com>
2023-05-10 02:23:45 -0300

    arreglo json vacio

--- ohmydog-client/src/pages/api/signup.ts
+++ ohmydog-client/src/pages/api/signup.ts
@@ -6,12 +6,14 @@ export default async function handler(
     res: NextApiResponse
 ) {
     try {
+        console.log("ABER SI ENTRA", req.body)
         const extRes = await axios.post(
-            'http://localhost:7162/api/register',
+            'http://localhost:7162/api/register/',
             req.body
         )
         res.status(200).json(extRes.data)
-    } catch (err) {
+    } catch (err) {     
+        console.log("ESTO ES EL ERROR XD ", err)
         try {
             const jsonSvRes = await axios.get('http://localhost:3001/signup')
             res.status(200).json(jsonSvRes.data)
