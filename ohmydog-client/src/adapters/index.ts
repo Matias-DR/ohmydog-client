6734f4388f65a6c11e06a2d9d4a78b68616f2953
Matias-DR <Matias-DR@outlook.com>
2023-05-10 02:22:34 -0300

    REF: signup body from form

--- ohmydog-client/src/adapters/index.ts
+++ ohmydog-client/src/adapters/index.ts
@@ -1,3 +1,1 @@
-export { default as createPetAdapter } from './create-pet.adapter'
-export { default as createUserAdapter } from './create-user.adapter'
 export { default as createSessionAdapter } from './create-session.adapter'
